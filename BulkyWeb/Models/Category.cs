bb7d9f8260852bac058353041fd51523b03d6b51
unknown <rasel mahmud>
2024-10-17 16:13:52 +0600

    CRUD operation

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+         public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Name field is required.")]
+        [MaxLength(100)]    
+        public string Name { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Display Order must be greater than 0.")]
+        public int DisplayOrder {  get; set; }  
+    }
+}
